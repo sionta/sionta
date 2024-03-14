03f48dbc444fc036b9feb85b4d9260aa3df8aa6d
Andre Attamimi <73837775+sionta@users.noreply.github.com>
2024-03-14 20:35:11 +0900

    Update test-pages.bat

--- tools/test-pages.bat
+++ tools/test-pages.bat
@@ -2,7 +2,7 @@
 if not exist "%~dp0..\_config.yml" ( exit /b 1 ) else ( cd /d "%~dp0.." )
 if not exist assets\lib\package.json git submodule update --init --recursive
 start "Jekyll Server" bundle exec jekyll serve --open-url --livereload
-exit /b %errolevel%
+exit /b %errorlevel%
 
 :: gem install bundle
 :: bundle install
