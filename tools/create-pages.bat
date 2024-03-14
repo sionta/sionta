dbfdb4b3f5e933a5b061d51db71203ab7b025400
Andre Attamimi <73837775+sionta@users.noreply.github.com>
2024-03-14 22:08:47 +0900

    Update create-pages.bat

--- tools/create-pages.bat
+++ tools/create-pages.bat
@@ -1,15 +1,18 @@
 @echo off
-if "%~1"=="" exit /b 1
-
-setlocal enabledelayedexpansion
+setlocal enableextensions
 
 set "baseName=%~n1"
 set "nowDate=%date%"
 set "formatDate="
 
-echo:%nowDate%| >nul findstr "^[0-9][0-9]/[0-9][0-9]/[0-9][0-9][0-9][0-9]$"
+if not defined baseName (
+   echo Error: No base name value.
+   exit /b 1
+)
+
+echo:%nowDate%|>nul findstr "^[0-9][0-9]/[0-9][0-9]/[0-9][0-9][0-9][0-9]$"
 if %errorlevel% equ 0 set "formatDate=dd/mm/yyyy"
-echo:%nowDate%| >nul findstr "^[0-9][0-9][0-9][0-9]/[0-9][0-9]/[0-9][0-9]$"
+echo:%nowDate%|>nul findstr "^[0-9][0-9][0-9][0-9]/[0-9][0-9]/[0-9][0-9]$"
 if %errorlevel% equ 0 set "formatDate=yyyy/mm/dd"
 
 if not defined formatDate exit /b 1
@@ -30,7 +33,7 @@ for /f "tokens=1-3 delims=/- " %%a in ("%nowDate%") do (
 set "fileName=%~dp0..\_posts\%year%-%month%-%day%-%baseName%.md"
 
 if exist "%fileName%" (
-    echo Error: File name '%baseName%' already exist.
+    echo Error: Base name '%baseName%' already exist.
     exit /b 1
 )
 
@@ -39,6 +42,8 @@ set "titleName=%titleName:_= %"
 set "titleName=%titleName:-= %"
 set "titleName=%titleName:\= %"
 set "titleName=%titleName:/= %"
+
+@rem TODO: add codepage restore use 'for /f 'command
 
 >"%fileName%" (
     chcp 65001 >nul
