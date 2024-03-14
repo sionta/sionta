28ce7cf6bfea267747ca9ed0893200cbc21779af
Andre Attamimi <73837775+sionta@users.noreply.github.com>
2024-03-15 06:16:43 +0900

    Update tool for test site on local

--- tools/test-site.bat
+++ tools/test-site.bat
@@ -1,8 +1,10 @@
 @echo off
 if not exist "%~dp0..\_config.yml" ( exit /b 1 ) else ( cd /d "%~dp0.." )
-if not exist assets\lib\package.json git submodule update --init --recursive
 start "Jekyll Server" bundle exec jekyll serve --open-url --livereload
 exit /b %errorlevel%
 
-:: gem install bundle
-:: bundle install
+:update
+git submodule update --init --recursive
+gem update
+gem install jekyll bundler && bundle
+bundle update
