c42b4ab19a1b44d8f73190a71e7388be8bac39d9
Andre Attamimi <73837775+sionta@users.noreply.github.com>
2024-03-23 03:36:01 +0900

    Update sync all forked GitHub repos

---
+++ tools/sync-forks.ps1
@@ -0,0 +1,26 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true, Position = 0)]
+    [Alias('u', 'owner')]
+    [string]$userName,
+    [Parameter(ValueFromPipeline = $true)]
+    [Alias('e', 'ignore')]
+    [string[]]$Excludes,
+    [Alias('f', 'force')]
+    [switch]$hardReset
+)
+
+try {
+    $repos = & gh repo list $userName --fork --json name --jq '.[].name'
+    $params = if ($hardReset) { '--force' } else { $null }
+    foreach ($repo in $repos) {
+        if ($repo -notin $Excludes) {
+            Write-Host "Synchronizing '$userName/$repo'..."
+            & gh repo sync "$userName/$repo" $params
+        }
+    }
+} catch {
+    $errorMessage = $_.Exception.Message
+    Write-Error "Error during synchronization: $errorMessage"
+    exit 1
+}
