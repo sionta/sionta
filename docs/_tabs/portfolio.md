b4f1dc695aeb5acb7f085a5734b305934c332af0
Andre Attamimi <73837775+sionta@users.noreply.github.com>
2024-03-23 03:40:58 +0900

    Reinitialized sites
    The startup template for Chirpy https://github.com/cotes2020/chirpy-starter

--- docs/_tabs/portfolio.md
+++
@@ -1,5 +0,0 @@
----
-layout: portfolio
-icon: fas fa-project-diagram
-order: 1
----
--- tools/create-post.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-if "%~1"=="/?" (
-    powershell -nop -ex unrestricted -c help "%~dpn0.ps1" -full
-) else (
-    powershell -nop -ex unrestricted -f "%~dpn0.ps1" %*
-)
