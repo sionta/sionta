1e8112ad3968f25648ef8fb8e30863284f5cecf4
Andre Attamimi <73837775+sionta@users.noreply.github.com>
2024-07-19 23:56:56 +0900

    sdfgh

--- scripts/sync-forks.ps1
+++ scripts/sync-forks.ps1
@@ -2,10 +2,16 @@
 param (
     [Parameter(Mandatory = $true, Position = 0)]
     [Alias('u', 'owner')]
+
     [string]$userName,
     [Parameter(ValueFromPipeline = $true)]
+
     [Alias('e', 'ignore')]
     [string[]]$Excludes,
+
+    [Alias('c', 'clone')]
+    [switch]$cloneRepo,
+
     [Alias('f', 'force')]
     [switch]$hardReset
 )
@@ -15,11 +21,27 @@ try {
     $params = if ($hardReset) { '--force' } else { $null }
     foreach ($repo in $repos) {
         if ($repo -notin $Excludes) {
-            Write-Host "Synchronizing '$userName/$repo'..."
-            gh repo sync "$userName/$repo" $params
+            if ($cloneRepo) {
+                if (Test-Path "$repo" -PathType Container) {
+                    Write-Host "Fetch and pulling '$userName/$repo'..."
+                    Push-Location "$repo"
+                    git fetch && git pull
+                    Pop-Location
+                }
+                else {
+                    Write-Host "Cloning '$userName/$repo'..."
+                    gh repo clone "$userName/$repo"
+                }
+            }
+            else {
+                Write-Host "Synchronizing '$userName/$repo'..."
+                gh repo sync "$userName/$repo" $params
+
+            }
         }
     }
-} catch {
+}
+catch {
     $errorMessage = $_.Exception.Message
     Write-Error "Error during synchronization: $errorMessage"
     exit 1
